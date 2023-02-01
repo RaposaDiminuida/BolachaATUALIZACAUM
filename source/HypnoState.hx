33d6c630bfca1d23d8945bd5ab2fa65dfad0900c
RaposaDiminuida <124216155+RaposaDiminuida@users.noreply.github.com>
2023-02-01 20:30:57 -0300

    Initial commit

---
+++ source/HypnoState.hx
@@ -0,0 +1,65 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxSubState;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import flixel.effects.FlxFlicker;
+import lime.app.Application;
+import flixel.addons.transition.FlxTransitionableState;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxTimer;
+
+class HypnoState extends MusicBeatState
+{
+	public static var leftState:Bool = false;
+
+	var warnText:FlxText;
+	override function create()
+	{
+		super.create();
+
+		var bg:FlxSprite = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
+		add(bg);
+
+		warnText = new FlxText(0, 0, FlxG.width,
+			"Sup bro, im here to give this   \n
+			i found this in a abandoned house \n
+			just get it\n
+			Press ESCAPE to NO.\n
+			Press ENTER to YES.\n
+			\n
+			The Cartridge Guy",
+			32);
+		warnText.setFormat("VCR OSD Mono", 32, FlxColor.WHITE, CENTER);
+		warnText.screenCenter(Y);
+		add(warnText);
+	}
+
+	override function update(elapsed:Float)
+	{
+		if(!leftState) {
+			if (controls.ACCEPT) {
+				leftState = true;
+                                PlayState.SONG = Song.loadFromJson('amogus-hard', 'amogus');
+                                LoadingState.loadAndSwitchState(new PlayState());
+			}
+			else if(controls.BACK) {
+				leftState = true;
+			}
+
+			if(leftState)
+			{
+				FlxG.sound.play(Paths.sound('cancelMenu'));
+				FlxTween.tween(warnText, {alpha: 0}, 1, {
+					onComplete: function (twn:FlxTween) {
+						MusicBeatState.switchState(new MainMenuState());
+					}
+				});
+			}
+		}
+		super.update(elapsed);
+	}
+}
+
